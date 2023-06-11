d2148a3831d0e15f4483bd10073f005015ac5da2
Ivan Zarzhitski <ivan.zarzhitski@gmail.com>
2023-06-12 01:27:52 +0200

    scrollbar but map width?

--- Board.py
+++ Board.py
@@ -2,7 +2,6 @@ import pygame
 import pygame_menu
 import sys
 
-
 class Board:
     # gui params
     main_window_size = main_window_width, main_window_height = 1200, 420
@@ -29,6 +28,12 @@ class Board:
         self.scroll_vel = scroll_vel
         self.clock = pygame.time.Clock()
         self.font = pygame.font.Font(None, legend_font_size)
+        self.scrollbar_x = 0
+        self.scrollbar_width = 100
+        self.scrollbar_height = 20
+        self.scrollbar_pressed = False
+        self.scrollbar_mult = 0
+
 
     def init_map(self):
         with open("map/map0", "r") as f:
@@ -53,13 +58,25 @@ class Board:
         for i in range(1, len(self.legend_elems)):
             pygame.draw.rect(self.sub_screen, self.colors[i], pygame.Rect(150, 10 + 30*i, 20, 20))
 
+    def is_click_inside_scrollbar(self, event):
+        return event.pos[0] >= self.scrollbar_x and event.pos[
+            0] <= self.scrollbar_x + self.scrollbar_width and self.main_window_height - self.scrollbar_height <= \
+            event.pos[1] and self.main_window_height >= event.pos[1]
+
     def start(self):
         self.init_map()
         self.main_screen = pygame.display.set_mode(self.main_window_size)
         self.sub_screen = pygame.Surface(self.sub_window_size)
         self.sub_screen.fill((255, 255, 255))
         self.create_legend()
+
+        self.total_width = self.map_w * self.cell_size
+        # self.scrollbar_width = self.total_width /
+        self.scrollbar_mult = (self.total_width - self.main_window_width) / (
+                    self.main_window_width - self.scrollbar_width)
+
         self.main_loop()
+
 
     def main_loop(self):
         while True:
@@ -68,6 +85,21 @@ class Board:
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if event.button == 1 and self.is_click_inside_scrollbar(event):
+                        self.scrollbar_pressed = True
+                        print(event.pos[0], event.pos[1])
+                if event.type == pygame.MOUSEBUTTONUP:
+                    if event.button == 1:
+                        self.scrollbar_pressed = False
+                if event.type == pygame.MOUSEMOTION:
+                    if self.scrollbar_pressed:
+                        self.scrollbar_x += event.rel[0]
+                        self.scrollbar_x = max(0, self.scrollbar_x)
+                        self.scrollbar_x = min(self.main_window_width - self.scrollbar_width, self.scrollbar_x)
+                        self.scroll_x = - self.scrollbar_x * self.scrollbar_mult
+
+
             # moving screen with arrows
             keys = pygame.key.get_pressed()
             if keys[pygame.K_LEFT] and self.scroll_x < 0:
@@ -85,9 +117,12 @@ class Board:
             # drawing subscreen with legend
             self.main_screen.blit(self.sub_screen, ((self.main_window_width - self.sub_window_width) // 2,
                                                     self.main_window_height - self.sub_window_height))
+            pygame.draw.rect(self.main_screen, (0, 0, 0), pygame.Rect(self.scrollbar_x,
+                                                                      self.main_window_height - self.scrollbar_height,
+                                                                      self.scrollbar_width,
+                                                                      self.scrollbar_height))
             # todo figure out how to draw legend only once
             pygame.display.flip()
-
 
 if __name__ == '__main__':
     my_board = Board()
