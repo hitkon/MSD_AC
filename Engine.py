23dbc85da7b1e68c025423bcecb68f4c9891d906
MCzech3605 <mczech@student.agh.edu.pl>
2023-06-16 12:09:13 +0200

    Spawning Budryka and Kawiory cars

--- Engine.py
+++ Engine.py
@@ -1,6 +1,12 @@
 from collections import namedtuple
+from random import randint
+from Participants import *
 
 Fraction = namedtuple('Fraction', ['numerator', 'denominator'])
+
+
+def rand_with_probability(prob: Fraction):
+    return randint(1, prob[1]) <= prob[0]
 
 
 class Engine:
@@ -14,9 +20,12 @@ class Engine:
         self.map_h = map_h
         self.map_w = map_w
         self.iter_counter = 0
+        self.budryka_cars = [[], []]
+        self.kawiory_cars = [[], []]
 
     def iteration(self):    # todo
         self.spawn_cars()
+        self.iter_counter += 1
 
     def get_map(self):      # todo
         return [[0 for _ in range(self.map_h)] for i in range(self.map_w)]
@@ -25,6 +34,24 @@ class Engine:
         return True
 
     def spawn_cars(self):                     # todo
+        prob_budryka_spawn = Fraction(14, 1000)
+        if rand_with_probability(prob_budryka_spawn):
+            car_scooter_prob = Fraction(10, 13)
+            initial_pos = (669, 70)
+            if rand_with_probability(car_scooter_prob):
+                veh = Car(initial_pos)
+            else:
+                veh = Scooter(initial_pos)
+            self.budryka_cars[1].append(veh)
+        prob_kawiory_spawn = Fraction(1, 300)
+        if rand_with_probability(prob_kawiory_spawn):
+            car_scooter_prob = Fraction(2, 3)
+            initial_pos = (751, 70)
+            if rand_with_probability(car_scooter_prob):
+                veh = Car(initial_pos)
+            else:
+                veh = Scooter(initial_pos)
+            self.kawiory_cars[1].append(veh)
         if not self.is_occupied(0, 1):
             pass
 
